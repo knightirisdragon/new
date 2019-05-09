b96fd916e0cb4661f07416bfcea779b3bf612a8d
Glitch (peeky) <none>
2019-05-09 20:39:24 +0000

    🏢🍕 Checkpoint ./public/server.js:1110799/25 ./public/commands.html:1110799/46

--- public/server.js
+++ public/server.js
@@ -4830,7 +4830,7 @@ if  (message.content.startsWith(peeky.serverData.get(keySF, "prefix") + "play ")
           
             var connection = await voiceChannel.join();
 
-            connection.playOpusStream(await ytdl_discord(GivenSong).catch(error => ErrorBag.add(error)))
+            connection.playOpusStream(await ytdl_discord(GivenSong, {  type: 'audioonly'  }).catch(error => ErrorBag.add(error)))
             .on('end', () => {
                const embed = {"description": InfoIcon + " The song has now finished with " + voiceChannel.members.filter(m => !m.user.bot).size + " listeners.",  "color": EmbedColor}; 
                message.channel.send({ embed }).catch(error => ErrorBag.add(error));
