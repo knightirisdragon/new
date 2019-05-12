09fe18f12faf0bb1ca0a9a9ee2202859fea3a1f9
Glitch (peeky) <none>
2019-05-12 06:26:10 +0000

    🔔💐 Checkpoint ./public/server.js:1110799/1043 ./public/index.html:1110799/140

--- public/server.js
+++ public/server.js
@@ -814,36 +814,6 @@ return List.join("");
 
 };
 
-function AddBookmark(a)  {
-  
-  var pageTitle=document.title;
-	var pageURL=document.location;
-	try {
-		// Internet Explorer solution
-		eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g,''));
-	}
-	catch (e) {
-		try {
-			// Mozilla Firefox solution
-			window.sidebar.addPanel(pageTitle, pageURL, "");
-		}
-		catch (e) {
-			// Opera solution
-			if (typeof(opera)=="object") {
-				a.rel="sidebar";
-				a.title=pageTitle;
-				a.url=pageURL;
-				return true;
-			} else {
-				// The rest browsers (i.e Chrome, Safari)
-				alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
-			}
-		}
-	}
-	return false;
-
-};
-
 function hideshowtopnav() {
    var x = document.getElementById("topnavmenu");
    var y = document.getElementById("hideshowbutton");
