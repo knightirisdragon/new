6359243cde39317bc5d341572461e635e9a3870d
Glitch (peeky) <none>
2019-05-19 15:52:59 +0000

    💳😀 Checkpoint ./public/index.html:1110799/314 ./public/server.js:1110799/37

--- public/server.js
+++ public/server.js
@@ -1579,7 +1579,7 @@ if  (!WebsiteCooldowns.has("staff"))  {
     };
     });
 
-    await fs.writeFile('public/staff.txt', '<font size="5" class="headertext">Developers</font>  <br>  <font size="2">These are the people that work on PEEKY to make him the best possible Discord bot that it is today.</font>  <br><br>  ' + DevList.join(" ") + '<br><br>  <font size="5" class="headertext">Helpers</font>  <br>  <font size="2">These are the people that can help you fix some PEEKY related issues.</font>  <br><br>' + HelperList.join(" ") + '<br><br>  <font size="5" class="headertext">Moderators</font>  <br>  <font size="2">These are the people that moderate the Support Server to keep it safe.</font>  <br><br>' + ModList.join(" "), (err) => {
+    await fs.writeFile('public/staff.txt', '<font size="5" class="headertext">Developers</font>  <br>  <font size="2">These are the people that work on PEEKY to make him the best possible Discord bot that it is today.</font>  <br><br>  <div class="staffroom">  ' + DevList.join(" ") + '  <>  <br><br>  <font size="5" class="headertext">Helpers</font>  <br>  <font size="2">These are the people that can help you fix some PEEKY related issues.</font>  <br><br>' + HelperList.join(" ") + '<br><br>  <font size="5" class="headertext">Moderators</font>  <br>  <font size="2">These are the people that moderate the Support Server to keep it safe.</font>  <br><br>' + ModList.join(" "), (err) => {
         if (err) console.log(err);
     });
       
