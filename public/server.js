182431ed08d9f63c5ff449ef9f86d873b2f8d93a
Glitch (peeky) <none>
2019-05-19 13:20:58 +0000

    👘👫 Checkpoint ./public/server.js:1110799/85

--- public/server.js
+++ public/server.js
@@ -1481,7 +1481,7 @@ if  (!WebsiteCooldowns.has("news"))  {
           
     messages.forEach(m => {
         var Header = m.content.split("\n")[0];
-        var Body   = m.content.replace("\n", "<br>")
+        var Body   = m.content.replace("\n", "<br>").replace(Header, "");
 
         NewsList.push('<div class="newsitem">  <b class="newsheader">  ' + Function_RemoveFormatting(Header, "other") + '  </b>  <br>  <b class="newsbody">  ' + Body + '  </b>  </div>');
     });
