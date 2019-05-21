a1c39f96646dda91c699eb56545d4337fe1e77d2
Glitch (peeky) <none>
2019-05-21 20:31:43 +0000

    👌🛬 Checkpoint ./public/server.js:1110799/289 ./public/index.html:1110799/21 ./public/store.html:1110799/4

--- public/server.js
+++ public/server.js
@@ -1557,16 +1557,20 @@ if  (!WebsiteCooldowns.has("news"))  {
         var Header = m.content.split("\n")[0];
         var Body   = m.content.split("\n").join("<br>").replace(Header, "");
         var Image = [];
+        var PrefixImage = "";
       
         if  (m.attachments.size > 0)  {
+          
+            PrefixImage = "<br><br>";
+          
             m.attachments.forEach(a => {
-              
-            Image.push('<br><br>  <img class="newsimage" src="' + a.url + '">');
+
+            Image.push('<img class="newsimage" src="' + a.url + '">');
 
             });
         };
 
-        NewsList.push('<div class="newsitem">  <b class="newsheader">  ' + Function_RemoveFormatting(Header, "other", false) + '  </b>  <br>  <b class="newsauthor">  Posted by ' + m.author.tag + ' on  ' + function_DateFormat(m.createdAt) + '.  </b>  <br>  <b class="newsbody">  ' + Function_RemoveFormatting(Body, "other", false) + '  </b>  ' + Image.join(" ") + '  <br><br><br>  <a class="button" href="https://discordapp.com/channels/' + SupportServer + '/' + AnnouncementsChannel + '/' + m.id + '">Open in Discord</a>  </div>');
+        NewsList.push('<div class="newsitem">  <b class="newsheader">  ' + Function_RemoveFormatting(Header, "other", false) + '  </b>  <br>  <b class="newsauthor">  Posted by <font color="#7289DA">' + m.author.tag + '</font> on <font color="#7289DA">' + function_DateFormat(m.createdAt) + '</font>.  </b>  <br>  <b class="newsbody">  ' + Function_RemoveFormatting(Body, "other", false) + '  </b>  ' + PrefixImage + Image.join(" ") + '  <br><br><br>  <a class="button" href="https://discordapp.com/channels/' + SupportServer + '/' + AnnouncementsChannel + '/' + m.id + '">Open in Discord</a>  </div>');
     });
 
     await fs.writeFile('public/news.txt', NewsList.join("<br><br>"), (err) => {
