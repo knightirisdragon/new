19132114e223e6fbfbbfedfcbe49edb480f724c6
Glitch (peeky) <none>
2019-06-16 13:09:21 +0000

    ⛴🏉 Checkpoint ./public/index.html:1110799/115 ./package.json:1110799/1825 ./public/server.js:1110799/223

--- public/server.js
+++ public/server.js
@@ -4,11 +4,15 @@ const peeky   = new Discord.Client();
 
 //DDBL
 const { ddblAPI } = require('ddblapi.js');
-const ddbl = new ddblAPI("482945063282802698", process.env.DDBL_TOKEN);
+const ddbl        = new ddblAPI("482945063282802698", process.env.DDBL_TOKEN);
 
 //DBL
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.DBL_TOKEN, peeky);
+
+//BTS
+const BotList = require('botlist.space');
+const bts     = new BotList.Client({ id: "482945063282802698", botToken: process.env.BTS_TOKEN });
 
 //MUSIC
 const ytdl_discord = require('ytdl-core-discord');
