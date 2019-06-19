6e213c9a44c3aea0943d1638b62424586308b706
Glitch (peeky) <none>
2019-06-19 17:55:15 +0000

    🎌🎖 Checkpoint ./public/server.js:1110799/151

--- public/server.js
+++ public/server.js
@@ -2577,12 +2577,6 @@ if  (!user.bot && reaction.message.channel.id == AnnouncementsChannel && reactio
   
 //Workshop Response
 if  (reaction.message.channel.id == WorkshopChannel && reaction.emoji.name == "🏁" && reaction.message.author.id == OwnerId)  {
-  
-    const key2 = reaction.message.author.id;
-  
-    if  (peeky.userData.has(key2))  {
-        peeky.userData.math(key2, "+", 100, "Gredit");
-    };
 
     const embed = {"description": SuccessIcon + " Your submission in the Workshop has been accepted!",  "color": EmbedColor}; 
     reaction.message.author.send({ embed }).catch(error => ErrorBag.add(error)).catch(error => ErrorBag.add(error));
