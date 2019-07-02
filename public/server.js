5852409a320f4778cf51cf2fae5217b546ac58d7
Glitch (peeky) <none>
2019-07-02 19:09:33 +0000

    🌏🚈 Checkpoint ./public/server.js:1110799/61

--- public/server.js
+++ public/server.js
@@ -2573,7 +2573,7 @@ if  (!user.bot && reaction.message.channel.id == AnnouncementsChannel && reactio
 };
   
 //Workshop Response
-if  (reaction.message.channel.id == WorkshopChannel && reaction.emoji.name == "🏁" && reaction.message.author.id == OwnerId)  {
+if  (reaction.message.channel.id == WorkshopChannel && reaction.emoji.name == "🏁")  {
 
     const embed = {"description": SuccessIcon + " Your submission in the Workshop has been accepted!",  "color": EmbedColor}; 
     reaction.message.author.send({ embed }).catch(error => ErrorBag.add(error)).catch(error => ErrorBag.add(error));
