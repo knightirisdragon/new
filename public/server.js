bae5d5588df8de194e53cd4a993dc53c9be7b70a
Glitch (peeky) <none>
2020-01-20 14:40:31 +0000

    🎙🍚 Checkpoint ./public/server.js:1110799/144

--- public/server.js
+++ public/server.js
@@ -7740,10 +7740,9 @@ if (CommandName.startsWith("play"))  {
 
                         const stream = ytdl(peeky.serverData.get(keySF, "Queue")[0]);
                         const dispatcher = await connection.play(stream, StreamOptions);
+                        peeky.serverData.set(keySF, dispatcher, "Dispatcher");
 
                         dispatcher.on('finish', async reason => {
-
-                        console.log("yes");
                           
                             if  (peeky.serverData.get(keySF, "Queue").length > 0)  {
                                 peeky.serverData.get(keySF, "Queue").shift();
