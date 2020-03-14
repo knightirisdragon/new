75d7113b68f24b32e3ff8020ff721bf8c80b3090
Glitch (peeky) <none>
2020-03-14 16:56:46 +0000

    🔨🐺 Checkpoint ./public/server.js:1110799/360 ./public/index.html:1110799/3480

--- public/server.js
+++ public/server.js
@@ -31,7 +31,6 @@ const Setting = require('./setting.json');
 const http    = require('http');
 const express = require('express');
 const app     = express();
-const port    = process.env.PORT;
 
 app.use(express.static('public'));
 app.get('/', function(request, response) {
@@ -41,23 +40,6 @@ app.get('/', function(request, response) {
 app.listen(process.env.PORT);
   setInterval(() => { http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
-
-http.createServer((req, res) => {
-	let responseCode = 404;
-	let content = '404 Error';
-
-	if (req.url === '/') {
-		responseCode = 200;
-		content = fs.readFileSync('./index.html');
-	}
-
-	res.writeHead(responseCode, {
-		'content-type': 'text/html;charset=utf-8',
-	});
-
-	res.write(content);
-	res.end();
-})
 
 //Miscellaneous Packages
 const fs         = require('fs');
