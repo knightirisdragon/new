eb5e22d6fcbe8ca50eeeaaedcc85ae76d76c5254
Glitch (peeky) <none>
2020-06-06 13:18:26 +0000

    🎄📇 Checkpoint ./public/website.js:1110799/1689 ./public/pages/backgrounds.html:1110799/528 ./public/pages/badges.html:1110799/536 ./public/pages/functions.html:1110799/535 ./public/pages/challenges.html:1110799/535 ./public/pages/commands.html:1110799/536 ./public/pages/invite.html:1110799/537 ./public/pages/leaderboard.html:1110799/567 ./public/pages/minigames.html:1110799/568 ./public/pages/randomsongs.html:1110799/537 ./public/pages/server.html:1110799/535 ./public/pages/serverlog.html:1110799/535 ./public/pages/servertemplate.html:1110799/535 ./public/pages/store.html:1110799/535 ./public/pages/translationsheet.html:1110799/535 ./public/pages/tutorials.html:1110799/536 ./public/pages/walloffame.html:1110799/560

--- public/website.js
+++ public/website.js
@@ -11,3 +11,23 @@ function UpdateWebsiteStuff(data)  {
 
 };
 
+//Fix Capitalization
+function function_FixCapitalization(text)  {
+
+    return text.slice(0, 1).toUpperCase() + text.slice(1, text.length).toLowerCase();
+      
+};
+
+//Head HTML
+
+//Title
+document.title = "PEEKY" + function_FixCapitalization(window.location.href.replace("https://peeky.glitch.me/pages/", "").replace(".html", ""));
+
+//Favicon
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://cdn.glitch.com/b2a48499-dec5-4ba6-898e-ec1e602d6eb9%2Fpeeky%204-2-1.png.png?1553621395208';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
