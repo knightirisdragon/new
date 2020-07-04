0fe2997efa88e9b8e5797576cc5a2744b8b96dec
Glitch (peeky) <none>
2020-07-04 22:39:44 +0000

    Import of Vojtasonic2/peeky

---
+++ public/website.js
@@ -0,0 +1,59 @@
+function UpdateWebsiteStuff(data)  {
+  
+    data.forEach(item => {
+      
+        fetch(Domain + '/website_data/' + item[0])
+        .then(response => response.text()).then((data) => {
+           document.getElementById(item[1]).innerHTML = data;
+        });
+      
+    });
+
+};
+
+//Fix Capitalization
+function FixTitle(text)  {
+  
+    text = text.split("#")[0].replace(Domain + "/pages/", "");
+  
+    var index = 0;
+    var Array = [];
+    var Words = text.split("_");
+  
+    Words.forEach(w => {
+      
+        w = w.replace("/", "").replace("peeky", "PEEKY");
+      
+        if  (w.toUpperCase() !== w && (w.length >= 3 || index == 0))  {
+          
+            Array.push(w.slice(0, 1).toUpperCase() + w.slice(1, w.length).toLowerCase());
+          
+        } else {
+          Array.push(w);          
+        };
+      
+        index ++;
+      
+    });
+
+    return Array.join(" ");
+      
+};
+
+var Domain = "https://peeky--vojtech-jilovec.repl.co";
+
+//Title
+if  (window.location.href == Domain + "/")  {
+    document.title = "PEEKY the Discord bot";
+} else {
+  document.title = "PEEKY – " + FixTitle(window.location.href);
+};
+
+//Favicon
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://cdn.glitch.com/b2a48499-dec5-4ba6-898e-ec1e602d6eb9%2Fpeeky%204-2-1.png.png?1553621395208';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
